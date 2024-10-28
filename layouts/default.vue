bff01d3f0c6165ebf40c951e43325c29fb0fc4e1
Raimisjes <raimisjes@gmail.com>
2024-10-28 15:29:06 +0200

    default layout added

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="container">
+    <PageHeader />
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1200px;
+  padding: 0 15px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-rows: 70px calc(100vh - 110px) 40px;
+  min-height: 100vh;
+
+  main {
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup></script>
+
 <template>
-    <PageHeader />
+  <div>homepage content</div>
 </template>
+
+<style lang="scss" scoped></style>
