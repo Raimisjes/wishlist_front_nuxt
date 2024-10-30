9df6c63eeea4c9db84316ccfa3254bb7db9d638f
Raimisjes <raimisjes@gmail.com>
2024-10-30 12:03:35 +0200

    config fixes(ts)

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,17 @@ module.exports = {
     'eslint:recommended',
     'plugin:nuxt/recommended',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+  },
+  plugins: ['vue', '@typescript-eslint'],
 };
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,4 +47,7 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  typescript: {
+    strict: true,
+  },
 });
