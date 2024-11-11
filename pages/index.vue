2f033830f4c6f93f14597ca0e2221b3062d006ae
Raimisjes <raimisjes@gmail.com>
2024-11-11 18:34:04 +0200

    homepage created

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,178 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
+import { useTemplateRef } from 'vue';
+import { useHomepageStore } from '@/stores/homepage';
+
+const { locale, t } = useI18n();
+
+const store = useHomepageStore();
+
+const usernameCheckFormEl = useTemplateRef('usernameCheckFormEl');
+
+const usernameCheck = {
+  rules: {
+    minLength: (v: String) =>
+      v.length >= 3 || t('errors.homepage.usernameCheck.minLength'),
+    match: (v: String) =>
+      v.match(/^[a-zA-Z0-9_.-]{3,20}$/) != null ||
+      t('errors.homepage.usernameCheck.wrongFormat'),
+  },
+};
+
+function submitForm() {
+  if (!usernameCheckFormEl.value?.isValid) return;
+  store.checkUsername();
+}
+
+function onInputUsername() {
+  store.state.usernameCheck.usernameExists = null;
+  store.state.usernameCheck.form.error = '';
+}
+</script>
 
 <template>
-  <div>homepage content</div>
+  <main>
+    <div class="left-side">
+      <h1 v-html="$t('pages.index.slogan')"></h1>
+      <v-form @submit.prevent="submitForm()" ref="usernameCheckFormEl">
+        <v-text-field
+          v-model="store.state.usernameCheck.form.username"
+          @input="onInputUsername()"
+          @click:append-inner="submitForm()"
+          :loading="store.state.usernameCheck.form.isLoading"
+          :disabled="store.state.usernameCheck.form.isLoading"
+          :prefix="$t('pages.index.usernameCheckPrefix')"
+          :error-messages="
+            store.state.usernameCheck.form.error != ''
+              ? $t(store.state.usernameCheck.form.error)
+              : ''
+          "
+          :placeholder="$t('pages.index.usernameCheckPlaceholder')"
+          :rules="[usernameCheck.rules.minLength, usernameCheck.rules.match]"
+          variant="underlined"
+          color="primary"
+          theme="default"
+          append-inner-icon="mdi-magnify"
+        ></v-text-field>
+        <Transition name="fade">
+          <div
+            class="button-holder"
+            v-if="store.state.usernameCheck.usernameExists == false"
+            mode="in-out"
+          >
+            <v-btn
+              color="primary"
+              to="/registration"
+              size="small"
+              :title="$t('words.signup')"
+              >{{ $t('words.signup') }}</v-btn
+            >
+          </div>
+        </Transition>
+      </v-form>
+    </div>
+    <!-- <div class="hero-image-holder"></div> -->
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+  .left-side {
+    padding: 0 30px 0 0;
+
+    h1 {
+      font-size: 70px;
+      font-weight: 700;
+      margin: 0 0 30px 0;
+      color: var(--text-color);
+      line-height: 1.5;
+    }
+
+    form {
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: space-between;
+      align-items: flex-start;
+      height: 110px;
+
+      .v-input {
+        width: 100%;
+        max-width: calc(100% - 135px);
+        max-height: 77px;
+      }
+      .button-holder {
+        padding: 20px 0 0 15px;
+
+        .v-btn {
+          width: 120px;
+
+          .v-btn__content,
+          .v-btn__prepend {
+            color: #fff;
+            font-weight: 600;
+            text-transform: lowercase;
+          }
+        }
+      }
+    }
+  }
+
+  .hero-image-holder {
+    min-width: 400px;
+    height: 400px;
+    background-image: url(@/assets/images/hero-image.png);
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+
+  @media screen and (max-width: 960px) {
+    .left-side {
+      h1 {
+        font-size: 50px;
+      }
+    }
+
+    .hero-image-holder {
+      min-width: 300px;
+      height: 300px;
+    }
+  }
+
+  @media screen and (max-width: 750px) {
+    flex-flow: column-reverse nowrap;
+
+    .left-side {
+      padding: 0;
+      max-width: 350px;
+
+      h1 {
+        font-size: 32px;
+        text-align: center;
+      }
+      form {
+        flex-flow: column nowrap;
+
+        .v-input {
+          max-width: 100%;
+        }
+        .button-holder {
+          width: 100%;
+          padding: 5px 0 0 0;
+
+          .v-btn {
+            width: 100%;
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 450px) {
+    .hero-image-holder {
+      min-width: 250px;
+      height: 250px;
+    }
+  }
+}
+</style>
---
+++ stores/homepage.ts
@@ -0,0 +1,68 @@
+import { defineStore } from 'pinia';
+import { reactive } from 'vue';
+
+interface HomepageState {
+  usernameCheck: {
+    form: {
+      username: String;
+      isLoading: Boolean;
+      error: String;
+    };
+    usernameExists: null | Boolean;
+  };
+}
+
+interface UsernameCheckRes {
+  status: Boolean;
+  data: {
+    usernameExists: Boolean;
+  };
+}
+
+export const useHomepageStore = defineStore('homepage', () => {
+  const state = reactive<HomepageState>({
+    usernameCheck: {
+      form: {
+        username: '',
+        isLoading: false,
+        error: '',
+      },
+      usernameExists: null,
+    },
+  });
+
+  async function checkUsername() {
+    state.usernameCheck.usernameExists = null;
+
+    const activeForm = state.usernameCheck.form;
+    if (state.usernameCheck.form.isLoading) return;
+
+    state.usernameCheck.form.isLoading = true;
+    try {
+      const response = await $fetch<UsernameCheckRes>(
+        'http://localhost:5000/user/checkusername',
+        {
+          method: 'post',
+          body: {
+            username: state.usernameCheck.form.username,
+          },
+          timeout: 10000,
+        },
+      );
+      state.usernameCheck.usernameExists = response.data.usernameExists;
+    } catch (error) {
+      let errorMessage = '';
+      !error.data
+        ? (errorMessage = 'errors.internal001')
+        : (errorMessage = `errors.${error.data.data}`);
+
+      state.usernameCheck.form.error = errorMessage;
+    }
+    state.usernameCheck.form.isLoading = false;
+  }
+
+  return {
+    state,
+    checkUsername,
+  };
+});
